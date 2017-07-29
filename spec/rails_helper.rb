0c330b214aa62de423bca645cc285cc63bf9e449
newtonkiragu <karanunewton4@gmail.com>
2017-07-29 16:57:25 +0300

    wrote my first test and corrected a typo in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,7 +57,7 @@ RSpec.configure do |config|
 end
 
 Shoulda::Matchers.configure do |config|
-  config.intergrate do |with|
+  config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
